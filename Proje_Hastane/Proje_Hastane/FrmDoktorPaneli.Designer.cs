24753534c837e779e0e98c8f638fe81a82a3aacb
Hüseyin Karadana <huseyinkaradana35@gmail.com>
2023-05-05 17:10:45 +0300

    Add files via upload

---
+++ Proje_Hastane/Proje_Hastane/FrmDoktorPaneli.Designer.cs
@@ -0,0 +1,225 @@
+﻿namespace Proje_Hastane
+{
+    partial class FrmDoktorPaneli
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmDoktorPaneli));
+            this.label1 = new System.Windows.Forms.Label();
+            this.cbBrans = new System.Windows.Forms.ComboBox();
+            this.txtAd = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtSoyad = new System.Windows.Forms.TextBox();
+            this.mskTc = new System.Windows.Forms.MaskedTextBox();
+            this.txtSifre = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnEkle = new System.Windows.Forms.Button();
+            this.btnSil = new System.Windows.Forms.Button();
+            this.btnGuncelle = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(44, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 28);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ad:";
+            // 
+            // cbBrans
+            // 
+            this.cbBrans.FormattingEnabled = true;
+            this.cbBrans.Location = new System.Drawing.Point(106, 124);
+            this.cbBrans.Name = "cbBrans";
+            this.cbBrans.Size = new System.Drawing.Size(171, 36);
+            this.cbBrans.TabIndex = 3;
+            // 
+            // txtAd
+            // 
+            this.txtAd.Location = new System.Drawing.Point(106, 36);
+            this.txtAd.Name = "txtAd";
+            this.txtAd.Size = new System.Drawing.Size(171, 36);
+            this.txtAd.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(27, 227);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(62, 28);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Şifre:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(45, 180);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(44, 28);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "TC:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(17, 132);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(72, 28);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Branş:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(12, 80);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(77, 28);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "Soyad:";
+            // 
+            // txtSoyad
+            // 
+            this.txtSoyad.Location = new System.Drawing.Point(106, 80);
+            this.txtSoyad.Name = "txtSoyad";
+            this.txtSoyad.Size = new System.Drawing.Size(171, 36);
+            this.txtSoyad.TabIndex = 2;
+            // 
+            // mskTc
+            // 
+            this.mskTc.Location = new System.Drawing.Point(106, 172);
+            this.mskTc.Mask = "00000000000";
+            this.mskTc.Name = "mskTc";
+            this.mskTc.Size = new System.Drawing.Size(171, 36);
+            this.mskTc.TabIndex = 4;
+            this.mskTc.ValidatingType = typeof(int);
+            // 
+            // txtSifre
+            // 
+            this.txtSifre.Location = new System.Drawing.Point(106, 219);
+            this.txtSifre.Name = "txtSifre";
+            this.txtSifre.Size = new System.Drawing.Size(171, 36);
+            this.txtSifre.TabIndex = 5;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(303, 36);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(672, 345);
+            this.dataGridView1.TabIndex = 11;
+            this.dataGridView1.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellDoubleClick);
+            // 
+            // btnEkle
+            // 
+            this.btnEkle.Location = new System.Drawing.Point(106, 281);
+            this.btnEkle.Name = "btnEkle";
+            this.btnEkle.Size = new System.Drawing.Size(84, 42);
+            this.btnEkle.TabIndex = 6;
+            this.btnEkle.Text = "Ekle";
+            this.btnEkle.UseVisualStyleBackColor = true;
+            this.btnEkle.Click += new System.EventHandler(this.btnEkle_Click);
+            // 
+            // btnSil
+            // 
+            this.btnSil.Location = new System.Drawing.Point(195, 281);
+            this.btnSil.Name = "btnSil";
+            this.btnSil.Size = new System.Drawing.Size(82, 42);
+            this.btnSil.TabIndex = 7;
+            this.btnSil.Text = "Sil";
+            this.btnSil.UseVisualStyleBackColor = true;
+            this.btnSil.Click += new System.EventHandler(this.btnSil_Click);
+            // 
+            // btnGuncelle
+            // 
+            this.btnGuncelle.Location = new System.Drawing.Point(108, 339);
+            this.btnGuncelle.Name = "btnGuncelle";
+            this.btnGuncelle.Size = new System.Drawing.Size(169, 42);
+            this.btnGuncelle.TabIndex = 8;
+            this.btnGuncelle.Text = "Güncelle";
+            this.btnGuncelle.UseVisualStyleBackColor = true;
+            this.btnGuncelle.Click += new System.EventHandler(this.btnGuncelle_Click);
+            // 
+            // FrmDoktorPaneli
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 28F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(227)))), ((int)(((byte)(43)))), ((int)(((byte)(11)))));
+            this.ClientSize = new System.Drawing.Size(995, 401);
+            this.Controls.Add(this.btnGuncelle);
+            this.Controls.Add(this.btnSil);
+            this.Controls.Add(this.btnEkle);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.txtSifre);
+            this.Controls.Add(this.mskTc);
+            this.Controls.Add(this.txtSoyad);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtAd);
+            this.Controls.Add(this.cbBrans);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Corbel", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.MaximizeBox = false;
+            this.Name = "FrmDoktorPaneli";
+            this.Text = "Doktor İşlemleri";
+            this.Load += new System.EventHandler(this.FrmDoktorPaneli_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cbBrans;
+        private System.Windows.Forms.TextBox txtAd;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtSoyad;
+        private System.Windows.Forms.MaskedTextBox mskTc;
+        private System.Windows.Forms.TextBox txtSifre;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnEkle;
+        private System.Windows.Forms.Button btnSil;
+        private System.Windows.Forms.Button btnGuncelle;
+    }
+}
---
+++ Proje_Hastane/Proje_Hastane/FrmDoktorPaneli.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+namespace Proje_Hastane
+{
+    public partial class FrmDoktorPaneli : Form
+    {
+        public FrmDoktorPaneli()
+        {
+            InitializeComponent();
+        }
+        sqlbaglantisi bgl=new sqlbaglantisi();
+        public void dataTableFill()
+        {
+            
+            DataTable dt1 = new DataTable();
+            SqlDataAdapter da1 = new SqlDataAdapter("SELECT * FROM Tbl_Doktorlar", bgl.baglanti());
+            da1.Fill(dt1);
+            dataGridView1.DataSource = dt1;
+        }
+        private void FrmDoktorPaneli_Load(object sender, EventArgs e)
+        {
+            dataTableFill();
+            //bransları comboboxa aktarma
+            SqlCommand komut2 = new SqlCommand("SELECT BransAd FROM Tbl_Branslar", bgl.baglanti());
+            SqlDataReader dr2 = komut2.ExecuteReader();
+            while (dr2.Read())
+            {
+                cbBrans.Items.Add(dr2[0].ToString());
+            }
+        }
+
+        private void btnEkle_Click(object sender, EventArgs e)
+        {
+            SqlCommand komut = new SqlCommand("INSERT INTO Tbl_Doktorlar (DoktorAd,DoktorSoyad,DoktorBrans,DoktorTC,DoktorSifre) VALUES (@p1,@p2,@p3,@p4,@p5)",bgl.baglanti());
+            komut.Parameters.AddWithValue("@p1",txtAd.Text);
+            komut.Parameters.AddWithValue("@p2",txtSoyad.Text);
+            komut.Parameters.AddWithValue("@p3",cbBrans.Text);
+            komut.Parameters.AddWithValue("@p4",mskTc.Text);
+            komut.Parameters.AddWithValue("@p5",txtSifre.Text);
+            komut.ExecuteNonQuery();
+            bgl.baglanti().Close();
+            dataTableFill();
+            MessageBox.Show("Doktor Eklendi","Bilgi",MessageBoxButtons.OK,MessageBoxIcon.Information);
+        }
+
+        private void dataGridView1_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            int secilen = dataGridView1.SelectedCells[0].RowIndex;
+            txtAd.Text = dataGridView1.Rows[secilen].Cells[1].Value.ToString();
+            txtSoyad.Text= dataGridView1.Rows[secilen].Cells[2].Value.ToString();
+            cbBrans.Text= dataGridView1.Rows[secilen].Cells[3].Value.ToString();
+            mskTc.Text= dataGridView1.Rows[secilen].Cells[4].Value.ToString();
+            txtSifre.Text= dataGridView1.Rows[secilen].Cells[5].Value.ToString();
+        }
+
+        private void btnSil_Click(object sender, EventArgs e)
+        {
+            SqlCommand komut = new SqlCommand("DELETE FROM Tbl_Doktorlar WHERE DoktorTC=@p1",bgl.baglanti());
+            komut.Parameters.AddWithValue("@p1",mskTc.Text);
+            komut.ExecuteNonQuery();
+            bgl.baglanti().Close();
+            MessageBox.Show("Kayıt Silindi","Uyarı",MessageBoxButtons.OK,MessageBoxIcon.Asterisk);
+            dataTableFill();
+        }
+
+        private void btnGuncelle_Click(object sender, EventArgs e)
+        {
+            SqlCommand komut = new SqlCommand("UPDATE Tbl_Doktorlar SET DoktorAd=@p1,DoktorSoyad=@p2,DoktorBrans=@p3,DoktorSifre=@p5 WHERE DoktorTC=@p4",bgl.baglanti());
+            komut.Parameters.AddWithValue("@p1", txtAd.Text);
+            komut.Parameters.AddWithValue("@p2", txtSoyad.Text);
+            komut.Parameters.AddWithValue("@p3", cbBrans.Text);
+            komut.Parameters.AddWithValue("@p4", mskTc.Text);
+            komut.Parameters.AddWithValue("@p5", txtSifre.Text);
+            komut.ExecuteNonQuery();
+            bgl.baglanti().Close();
+            dataTableFill();
+            MessageBox.Show("Doktor Güncellendi", "Bilgi", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+    }
+}
